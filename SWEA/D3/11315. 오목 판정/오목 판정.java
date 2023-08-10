f14b3832a8a4dd7e491fc1a02c11d4efb2f329df
pado7sea <140683237+pado7sea@users.noreply.github.com>
2023-08-10 21:12:33 +0900

    [D3] Title: 오목 판정, Time: 158 ms, Memory: 25,640 KB -BaekjoonHub

--- SWEA/D3/11315. 오목 판정/오목 판정.java
+++ SWEA/D3/11315. 오목 판정/오목 판정.java
@@ -1,4 +1,3 @@
-import java.util.Arrays;
 import java.util.Scanner;
 
 public class Solution {
@@ -21,62 +20,64 @@ public class Solution {
 			// 돌이 다섯 개 이상 연속한 곳이 있으면 yes = 1;
 			int yes = 0;
 
+			// 행
+			for (int r = 0; r < N; r++) {
+				for (int c = 0; c < N; c++) {
+					if (str[r][c] == 'o') { // 행에서 o을 발견했으면 그 다음 확인할 수 있는 공간이 4개가 남았는지 확인부터 먼저 함.
+						if (N - c >= 5) {
+							if (str[r][c + 1] == 'o' && str[r][c + 2] == 'o' && str[r][c + 3] == 'o'
+									&& str[r][c + 4] == 'o') {
+								yes = 1;
+							}
+						}
+					}
+				}
+			}
 
-            //행
-            for(int r = 0; r < N; r++) {
-                for(int c = 0; c < N; c++) {
-                    if(str[r][c]=='o') { // 행에서 o을 발견했으면 그 다음 확인할 수 있는 공간이 4개가 남았는지 확인부터 먼저 함.
-                        if(N-c >= 5) {
-                            if(str[r][c+1] == 'o' && str[r][c+2] == 'o' && str[r][c+3] == 'o' && str[r][c+4] == 'o') {
-                                yes = 1;
-                            }
-                        }
-                    }
-                }
-            }
-            
-            //열 
-            for(int c = 0; c < N; c++) {
-                for(int r = 0; r < N; r++) {
-                    if(str[r][c]=='o') { // 열에서 o을 발견했으면 그 다음 확인할 수 있는 공간이 4개가 남았는지 확인부터 먼저 함.
-                        if(N-r >= 5) {
-                            if(str[r+1][c] == 'o' && str[r+2][c] == 'o' && str[r+3][c] == 'o' && str[r+4][c] == 'o') {
-                                yes = 1;
-                            }
-                        }
-                    }
-                }
-            }
-            
-            //대각선 
-            for(int r = 0; r < N; r++) {
-                for(int c = 0; c < N; c++) {
-                    
-                        if(str[r][c]=='o') { 
-                            if(N-r >= 5 && N-c >=5 ) {
-                                if(str[r+1][c+1] == 'o' && str[r+2][c+2] == 'o' && str[r+3][c+3] == 'o' && str[r+4][c+4] == 'o') {
-                                    yes = 1;
-                                }
-                            }
-                        }
-                    
-                   
-                        if(str[r][c]=='o') { 
-                            if(N-r >= 5 && c >=4) {
-                                if(str[r+1][c-1] == 'o' && str[r+2][c-2] == 'o' && str[r+3][c-3] == 'o' && str[r+4][c-4] == 'o') {
-                                    yes = 1;
-                                }
-                            }
-                        }
-                    
-                }
-            }
-            
-            if(yes == 1) {
-                System.out.println("#"+testcase+" YES");
-            }else {
-                System.out.println("#"+testcase+" NO");
-            }
+			// 열
+			for (int c = 0; c < N; c++) {
+				for (int r = 0; r < N; r++) {
+					if (str[r][c] == 'o') { // 열에서 o을 발견했으면 그 다음 확인할 수 있는 공간이 4개가 남았는지 확인부터 먼저 함.
+						if (N - r >= 5) {
+							if (str[r + 1][c] == 'o' && str[r + 2][c] == 'o' && str[r + 3][c] == 'o'
+									&& str[r + 4][c] == 'o') {
+								yes = 1;
+							}
+						}
+					}
+				}
+			}
+
+			// 대각선
+			for (int r = 0; r < N; r++) {
+				for (int c = 0; c < N; c++) {
+
+					if (str[r][c] == 'o') { // 왼쪽 위에서 오른쪽 아래로 내려가는 대각선
+						if (N - r >= 5 && N - c >= 5) { 
+							if (str[r + 1][c + 1] == 'o' && str[r + 2][c + 2] == 'o' && str[r + 3][c + 3] == 'o'
+									&& str[r + 4][c + 4] == 'o') {
+								yes = 1;
+							}
+						}
+					}
+
+					if (str[r][c] == 'o') { // 오른쪽 위에서 왼쪽 아래로 내려가는 대각선
+						if (N - r >= 5 && c >= 4) {
+							if (str[r + 1][c - 1] == 'o' && str[r + 2][c - 2] == 'o' && str[r + 3][c - 3] == 'o'
+									&& str[r + 4][c - 4] == 'o') {
+								yes = 1;
+							}
+						}
+					}
+
+				}
+			}
+
+			if (yes == 1) {
+				System.out.println("#" + testcase + " YES");
+			} else {
+				System.out.println("#" + testcase + " NO");
+			}
 
 		}
 	}
