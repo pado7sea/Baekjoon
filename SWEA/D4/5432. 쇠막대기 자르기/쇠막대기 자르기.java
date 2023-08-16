6f2e44ba46e90a33cbe3d843b4e7bd53d4b770b2
pado7sea <140683237+pado7sea@users.noreply.github.com>
2023-08-16 13:17:51 +0900

    [D4] Title: 쇠막대기 자르기, Time: 370 ms, Memory: 98,452 KB -BaekjoonHub

--- SWEA/D4/5432. 쇠막대기 자르기/쇠막대기 자르기.java
+++ SWEA/D4/5432. 쇠막대기 자르기/쇠막대기 자르기.java
@@ -20,21 +20,21 @@ public class Solution {
 				strArr[i] = str.substring(i, i + 1);
 			}
 			
-			// 아이디어 : 레이저 왼쪽에 안닫힌 괄호가 몇 개 있나 세면 레이저 하나가 쇠막대기 몇개를 자르는 지 알 수 있음
+			// 아이디어 : 쇠막대기 개수 + 레이저 왼쪽에 안 닫힌 괄호의 개수를 세면 레이저 하나가 쇠막대기 몇개를 자르는 지 알 수 있음
 
 			// 쇠막대기 개수를 res에 저장하자.
 			for(int i = 0; i <str.length();i++){
-				if(strArr[i].equals("(")) {
+				if(strArr[i].equals("(")) { // ( 가 등장할 때마다 카운트하고
 					res++;
 				}
-				if (strArr[i].equals("(")  && strArr[i + 1].equals(")")) { 
+				if (strArr[i].equals("(")  && strArr[i + 1].equals(")")) { // 레이저 나올 때는 빼준다.
 					res--;
 				}
 			}
 
 			// 여는 괄호가 나올 때마다 스택에 삽입, 닫는 괄호가 나올 때마다 삭제.
 			for (int i = 0; i < str.length(); i++) {
-				// "()" 레이저가 나올 경우에는 그동안 스택에 저장되어있던 여는 괄호의 개수 +1을 res에 저장한다.
+				// "()" 레이저가 나올 경우에는 그동안 스택에 저장되어있던 여는 괄호의 개수를 res에 저장한다.
 				if (strArr[i].equals("(")  && strArr[i + 1].equals(")")) { 
 					if(!stack.isEmpty())
 						res += stack.size();
@@ -53,4 +53,3 @@ public class Solution {
 		}
 	}
 }
-
