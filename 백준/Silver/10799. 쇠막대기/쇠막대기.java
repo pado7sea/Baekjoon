63486d785b5766e71a893754e4b3e1b865a5a06e
pado7sea <140683237+pado7sea@users.noreply.github.com>
2023-08-16 13:29:53 +0900

    [Silver II] Title: 쇠막대기, Time: 304 ms, Memory: 24496 KB -BaekjoonHub

---
+++ 백준/Silver/10799. 쇠막대기/쇠막대기.java
@@ -0,0 +1,49 @@
+import java.util.Scanner;
+import java.util.Stack;
+
+public class Main {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+
+		Stack<Integer> stack = new Stack<>();
+
+		int res = 0; // 잘려진 쇠막대기 조각의 총 개수
+
+		// 입력
+		String str = sc.next();
+		String[] strArr = new String[str.length()];
+		for (int i = 0; i < str.length(); i++) {
+			strArr[i] = str.substring(i, i + 1);
+		}
+
+		// 아이디어 : 쇠막대기 개수 + 레이저 왼쪽에 안 닫힌 괄호의 개수를 세면 레이저 하나가 쇠막대기 몇개를 자르는 지 알 수 있음
+
+		// 쇠막대기 개수를 res에 저장하자.
+		for (int i = 0; i < str.length(); i++) {
+			if (strArr[i].equals("(")) { // ( 가 등장할 때마다 카운트하고
+				res++;
+			}
+			if (strArr[i].equals("(") && strArr[i + 1].equals(")")) { // 레이저 나올 때는 빼준다.
+				res--;
+			}
+		}
+
+		// 여는 괄호가 나올 때마다 스택에 삽입, 닫는 괄호가 나올 때마다 삭제.
+		for (int i = 0; i < str.length(); i++) {
+			// "()" 레이저가 나올 경우에는 그동안 스택에 저장되어있던 여는 괄호의 개수를 res에 저장한다.
+			if (strArr[i].equals("(") && strArr[i + 1].equals(")")) {
+				if (!stack.isEmpty())
+					res += stack.size();
+			}
+
+			if (strArr[i].equals("(")) {
+				stack.push(i);
+			} else if (strArr[i].equals(")")) {
+				stack.pop();
+			}
+		}
+
+		System.out.println(res);
+
+	}
+}
