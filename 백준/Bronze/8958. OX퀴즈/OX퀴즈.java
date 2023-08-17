543764ab0ecab67c6893b35e0fffdc911e148dd0
pado7sea <140683237+pado7sea@users.noreply.github.com>
2023-08-18 01:02:45 +0900

    [Bronze II] Title: OX퀴즈, Time: 132 ms, Memory: 13368 KB -BaekjoonHub

--- 백준/Bronze/8958. OX퀴즈/OX퀴즈.java
+++ 백준/Bronze/8958. OX퀴즈/OX퀴즈.java
@@ -3,24 +3,24 @@ import java.util.Scanner;
 public class Main {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
-		int N = sc.nextInt();
 
-		for (int i = 0; i < N; i++) {
-			
-			int sum = 0;
-			int count = 0;
+		int T = sc.nextInt();	// 테스트 케이스
+
+		for (int tc = 1; tc <= T; tc++) {
 			String str = sc.next();
-			
-			for (int j = 0; j < str.length(); j++) {
-				if (str.charAt(j) == 'O') {
-					count++;
-					sum += count;
-				} else if (str.charAt(j) == 'X') {
-					count = 0;
+
+			int res = 0;	//점수의 합
+			int count = 0;	//특정 문제의 점수
+			for (int i = 0; i < str.length(); i++) {
+				char ch = str.charAt(i);
+				if (ch == 'O') {
+					count++;	//그 문제의 점수는 그 문제까지 연속된 O의 개수다. 
+					res += count; 
+				} else if (ch == 'X') { 
+					count = 0; //X가 나오면 다음 문제의 점수는 다시 0점으로 초기화
 				}
 			}
-			System.out.println(sum);
-
+			System.out.println(res);
 		}
 	}
 }
