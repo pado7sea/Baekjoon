d3887cd49f7dcbc2fe485a6c7dc6301bc6bef838
pado7sea <140683237+pado7sea@users.noreply.github.com>
2023-08-19 13:23:28 +0900

    [D2] Title: 달팽이 숫자, Time: 134 ms, Memory: 21,160 KB -BaekjoonHub

--- SWEA/D2/1954. 달팽이 숫자/달팽이 숫자.java
+++ SWEA/D2/1954. 달팽이 숫자/달팽이 숫자.java
@@ -2,61 +2,48 @@ import java.util.Scanner;
 
 public class Solution {
 	public static void main(String[] args) {
+
+		int[] dx = { 0, 1, 0, -1 };	// 델타 : 우 하 좌 상
+		int[] dy = { 1, 0, -1, 0 };
+
 		Scanner sc = new Scanner(System.in);
 		int T = sc.nextInt();
 
-		for (int testcase = 1; testcase <= T; testcase++) {
-			int N = sc.nextInt();
+		for (int tc = 1; tc <= T; tc++) {
+			int N = sc.nextInt();	// 달팽이 배열의 크기
+			
+			int n = 1;			// 달팽이의 숫자 시작은 1부터 빙글빙글 증가할 것임
+			int d = 0;			// 우/하/좌/상 어느 방향으로 증가할 것인지 진행방향을 정해주는 델타 내의 인덱스 
+			
+			int x = 0, y = 0;	// 현재 위치
+			int nx, ny;			// 다음 위치(임시)
+			
+			int[][] arr = new int[N][N]; // 달팽이 N*N 배열
 
-			int[][] snail = new int[N][N];
+			while (n <= N * N) {
 
-			// 시작은 1부터~
-			int num = 1;
+				arr[x][y] = n++;	// 현재 위치에 n을 입력하고 그 다음에는 n+1;
+				nx = x + dx[d];		// 다음 위치(임시) 
+				ny = y + dy[d];
 
-			// 델타 : 우 하 좌 상
-			int[][] drc = { { 0, 1 }, { 1, 0 }, { 0, -1 }, { -1, 0 } };
-			int d = 0;
-
-			// 현재 나의 좌표
-			int nr = 0;
-			int nc = 0;
-
-			while (num <= N * N) {
-
-				snail[nr][nc] = num;
-				num++;
-				
-				//임시로 다음으로 한 칸 옮겨서 범위내인지(범위가 아니면 꺾어야함), 0인지 확인(0이 아니면 꺾어야 함)
-				int tempnr = nr + drc[d][0];
-				int tempnc = nc + drc[d][1];
-				if (isRange2(tempnr, tempnc, N) && snail[tempnr][tempnc] == 0) {
-					nr = tempnr;
-					nc = tempnc;
-
-					//꺾어야 하면 d에 1을 추가해서 진행 방향을 바꿔준다.
-				} else {
-					d = (d + 1) % 4;
-					nr = nr + drc[d][0];
-					nc = nc + drc[d][1];
+				// 만약, 다음 위치가 N*N 배열 밖이거나 이미 달팽이숫자가 채워진 행이나 열을 만난다면 
+				// 델타 내의 인덱스인 d에 +1을 하여 진행 방향을 바꿔준다. 
+				if (nx < 0 || nx >= N || ny < 0 || ny >= N || arr[nx][ny] != 0) {
+					d = (d + 1) % 4;  // 계속 델타(원소가 4개) 내에서 순회할 수 있도록 나머지(%4)를 사용해준다.
 				}
 
+				x += dx[d];	// 다음 위치(확정)이 현재 위치가 된다.
+				y += dy[d];
 			}
 
-			// 출력^^
-			System.out.println("#" + testcase);
+			// 출력
+			System.out.println("#" + tc);
 			for (int i = 0; i < N; i++) {
 				for (int j = 0; j < N; j++) {
-					System.out.print(snail[i][j] + " ");
+					System.out.print(arr[i][j] + " ");
 				}
 				System.out.println();
 			}
 		}
-	}
-
-	// 범위 내에 있으면 true 반환하는 함수
-	public static boolean isRange2(int nr, int nc, int N) {
-		if (nr >= 0 && nr < N && nc >= 0 && nc < N)
-			return true;
-		return false;
 	}
 }
