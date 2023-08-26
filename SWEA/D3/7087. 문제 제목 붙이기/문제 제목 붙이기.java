a1cdfe41f6f2070d68e491f80b365c5e41bff3a7
pado7sea <140683237+pado7sea@users.noreply.github.com>
2023-08-26 22:59:21 +0900

    [D3] Title: 문제 제목 붙이기, Time: 103 ms, Memory: 18,572 KB -BaekjoonHub

--- SWEA/D3/7087. 문제 제목 붙이기/문제 제목 붙이기.java
+++ SWEA/D3/7087. 문제 제목 붙이기/문제 제목 붙이기.java
@@ -1,7 +1,6 @@
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.util.Arrays;
-import java.util.StringTokenizer;
 
 public class Solution {
 	public static void main(String[] args) throws Exception {
@@ -21,17 +20,17 @@ public class Solution {
 			int ans = 1; // 최대 문제 제목의 개수
 
 			for (int i = 1; i < N; i++) {
-				if (ch[0] != 'A') { //A가 없으면 제목으로 쓸 수 있는 게 없는 거.
-					ans = 0; 
+				if (ch[0] != 'A') { // A가 없으면 제목으로 쓸 수 있는 게 없는 거.
+					ans = 0;
 					break;
 				}
-				if(ch[i]-ch[i-1]==0) { //알파벳이 중복이면 포문 한번 건너뜀
+				if (ch[i] - ch[i - 1] == 0) { // 알파벳이 중복이면 포문 한번 건너뜀
 					continue;
 				}
-				if(ch[i]-ch[i-1]==1) { //바로 다음 순서의 알파벳이 나오면 제목개수 증가
+				if (ch[i] - ch[i - 1] == 1) { // 바로 다음 순서의 알파벳이 나오면 제목개수 증가
 					ans++;
 				}
-				if(ch[i]-ch[i-1]>1) {
+				if (ch[i] - ch[i - 1] > 1) {
 					break;
 				}
 			}
