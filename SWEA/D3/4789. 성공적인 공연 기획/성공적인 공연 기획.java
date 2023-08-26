b89060b366662227e5c52ed9777e9eb8a8ffc2a6
pado7sea <140683237+pado7sea@users.noreply.github.com>
2023-08-27 04:11:01 +0900

    [D3] Title: 성공적인 공연 기획, Time: 123 ms, Memory: 21,304 KB -BaekjoonHub

--- SWEA/D3/4789. 성공적인 공연 기획/성공적인 공연 기획.java
+++ SWEA/D3/4789. 성공적인 공연 기획/성공적인 공연 기획.java
@@ -11,8 +11,8 @@ public class Solution {
 			int count = 0; // 고용인 수
 			for(int i = 0;i<str.length();i++) {
 				if(stand < i) { //i번째 사람들이 일어나기 위한 기립관객수가 부족할 때. 
-					count += i - stand; //더 필요한 관객수만큼 고용인 고용
-					stand =i;
+					count += (i - stand); //더 필요한 관객수만큼 고용인 고용
+					stand += (i - stand); // 늘어난 고용인만큼 기립관객수 늘어남.
 				}
 				stand += str.charAt(i) -'0'; // i번째에 기립박수 하고 있는 사람들을 누적합으로 계산
 			}
