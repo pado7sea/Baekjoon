67067105b8a90add4bb1b574e90b8d9f51940e29
pado7sea <140683237+pado7sea@users.noreply.github.com>
2023-08-26 18:20:58 +0900

    [D2] Title: 파스칼의 삼각형, Time: 103 ms, Memory: 18,164 KB -BaekjoonHub

--- SWEA/D2/2005. 파스칼의 삼각형/파스칼의 삼각형.java
+++ SWEA/D2/2005. 파스칼의 삼각형/파스칼의 삼각형.java
@@ -1,6 +1,5 @@
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
-import java.util.StringTokenizer;
 
 public class Solution {
 	public static void main(String[] args) throws Exception {
