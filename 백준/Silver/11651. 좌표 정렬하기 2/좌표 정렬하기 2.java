ebf39e1be1da5bafb6365f58ffa122904bc0ad0d
pado7sea <140683237+pado7sea@users.noreply.github.com>
2023-08-28 21:25:41 +0900

    [Silver V] Title: 좌표 정렬하기 2, Time: 2308 ms, Memory: 78148 KB -BaekjoonHub

---
+++ 백준/Silver/11651. 좌표 정렬하기 2/좌표 정렬하기 2.java
@@ -0,0 +1,29 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Main {
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+		int N = Integer.parseInt(br.readLine());
+		int[][] arr = new int[N][2];
+		for (int i = 0; i < N; i++) {
+			StringTokenizer st = new StringTokenizer(br.readLine());
+			arr[i][0] = Integer.parseInt(st.nextToken());
+			arr[i][1] = Integer.parseInt(st.nextToken());
+		}
+		//람다식
+		Arrays.sort(arr, (a, b) -> {
+			if (a[1] == b[1]) {
+				return a[0] - b[0];
+			}
+			return a[1] - b[1];
+		});
+
+		for (int i = 0; i < N; i++) {
+			System.out.println(arr[i][0] + " " + arr[i][1]);
+		}
+	}
+}
