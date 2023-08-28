95bd3f9bb454f5c1f00165d07f01957c966b0e7a
pado7sea <140683237+pado7sea@users.noreply.github.com>
2023-08-28 23:50:28 +0900

    [Bronze II] Title: 바구니 뒤집기, Time: 80 ms, Memory: 11620 KB -BaekjoonHub

---
+++ 백준/Bronze/10811. 바구니 뒤집기/바구니 뒤집기.java
@@ -0,0 +1,32 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		int N = Integer.parseInt(st.nextToken());
+		int M = Integer.parseInt(st.nextToken());
+		int[] basket = new int[N + 1];
+		for (int x = 1; x < N + 1; x++) {
+			basket[x] = x;
+		}
+
+		for (int t = 1; t <= M; t++) {
+			st = new StringTokenizer(br.readLine());
+			int i = Integer.parseInt(st.nextToken());
+			int j = Integer.parseInt(st.nextToken());
+
+			for(int x = i; x<=j ;x++,j--) {
+				int temp = basket[x];
+				basket[x] = basket[j];
+				basket[j] = temp;
+			}
+
+		}
+		for (int x = 1; x < N + 1; x++) {
+			System.out.print(basket[x] + " ");
+		}
+	}
+}
