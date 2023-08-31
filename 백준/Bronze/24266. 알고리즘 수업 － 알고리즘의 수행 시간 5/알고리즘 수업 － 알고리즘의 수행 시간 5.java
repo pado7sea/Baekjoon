f362607b3a2bb1ca0af5ed8b741357851a0064c1
pado7sea <140683237+pado7sea@users.noreply.github.com>
2023-09-01 02:00:55 +0900

    [Bronze III] Title: 알고리즘 수업 - 알고리즘의 수행 시간 5, Time: 80 ms, Memory: 11484 KB -BaekjoonHub

---
+++ 백준/Bronze/24266. 알고리즘 수업 － 알고리즘의 수행 시간 5/알고리즘 수업 － 알고리즘의 수행 시간 5.java
@@ -0,0 +1,11 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class Main {
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		Long n = Long.parseLong(br.readLine());
+		System.out.println(n*n*n);
+		System.out.println('3');
+	}
+}
