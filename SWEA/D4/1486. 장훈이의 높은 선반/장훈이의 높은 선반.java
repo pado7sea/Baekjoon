414f7c695abd8d1b995e1603050f80d1b24bcf38
pado7sea <140683237+pado7sea@users.noreply.github.com>
2023-09-04 11:57:40 +0900

    [D4] Title: 장훈이의 높은 선반, Time: 141 ms, Memory: 21,124 KB -BaekjoonHub

--- SWEA/D4/1486. 장훈이의 높은 선반/장훈이의 높은 선반.java
+++ SWEA/D4/1486. 장훈이의 높은 선반/장훈이의 높은 선반.java
@@ -1,40 +1,49 @@
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
 public class Solution {
-	static int N, B, ans;
-	static int[] H;
+	static int N, B; // 점원 수(N), 선반 높이(B)를 저장하는 변수
+	static int[] heights; // 각 점원의 키를 저장하는 배열
+	static int min; // 높이 차이의 최소값을 저장하는 변수
 
 	public static void main(String[] args) throws Exception {
-		Scanner sc = new Scanner(System.in);
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-		int T = sc.nextInt();
+		int T = Integer.parseInt(br.readLine()); // 테케
 		for (int tc = 1; tc <= T; tc++) {
-			N = sc.nextInt();
-			B = sc.nextInt();
-			H = new int[N];
+			StringTokenizer st = new StringTokenizer(br.readLine());
+			N = Integer.parseInt(st.nextToken());
+			B = Integer.parseInt(st.nextToken());
+
+			st = new StringTokenizer(br.readLine());
+			heights = new int[N];
 			for (int i = 0; i < N; i++) {
-				H[i] = sc.nextInt();
+				heights[i] = Integer.parseInt(st.nextToken());
 			}
-			ans = Integer.MAX_VALUE;
-			powerset(0,0);
-			System.out.println("#" + tc + " " + (ans - B));
+
+			min = Integer.MAX_VALUE;
+			powerset(0, 0);
+			System.out.println("#" + tc + " " + (min - B));
+
 		} // tc
 
 	}// main
 
-	public static void powerset(int idx, int sum) {
-		if (sum > ans) //ans가 이미 더 작은 값이라면 밑에 있는 if문을 실행할 필요가 없으니까ㅇㅇ
-			return;
-
+	public static void powerset(int idx, int currentHeight) {
+		// 기저파트
 		if (idx == N) {
-			if (sum >= B) {
-				ans = Math.min(ans, sum);
+
+			// 현재 점원 키의 합이 선반 높이 이상이면
+			if (currentHeight >= B) {
+				min = Math.min(min, currentHeight); // 둘 중 최솟값으로 저장
 			}
 			return;
-		}
 
-		powerset(idx + 1, sum + H[idx]);
-		powerset(idx + 1, sum);
+		}
+		// 재귀파트
+		powerset(idx + 1, currentHeight + heights[idx]); // 현재 직원 추가해서 재귀호출
+		powerset(idx + 1, currentHeight); // 현재 직원 추가 안하고 재귀호출
 
 	}
 }
