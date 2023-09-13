61d1027b53dc6eabc7f5bdcf187473b1a4f011c0
pado7sea <140683237+pado7sea@users.noreply.github.com>
2023-09-14 02:47:40 +0900

    [Silver II] Title: 알고리즘 수업 - 너비 우선 탐색 1, Time: 1008 ms, Memory: 95216 KB -BaekjoonHub

--- 백준/Silver/24444. 알고리즘 수업 － 너비 우선 탐색 1/알고리즘 수업 － 너비 우선 탐색 1.java
+++ 백준/Silver/24444. 알고리즘 수업 － 너비 우선 탐색 1/알고리즘 수업 － 너비 우선 탐색 1.java
@@ -9,74 +9,74 @@ import java.util.StringTokenizer;
 
 public class Main {
 
-	static Queue<Integer> queue = new LinkedList<>();
-	static ArrayList<Integer>[] graph;
-	static boolean[] visited;
-	static int[] visitOrder;
+    static Queue<Integer> queue = new LinkedList<>();
+    static ArrayList<Integer>[] graph;
+    static boolean[] visited;
+    static int[] visitOrder;
 
-	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st = new StringTokenizer(br.readLine());
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
 
-		int N = Integer.parseInt(st.nextToken());
-		int M = Integer.parseInt(st.nextToken());
-		int R = Integer.parseInt(st.nextToken());
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
+        int R = Integer.parseInt(st.nextToken());
 
-		graph = new ArrayList[N + 1];
-		visited = new boolean[N + 1];
-		visitOrder = new int[N + 1];
+        graph = new ArrayList[N + 1];
+        visited = new boolean[N + 1];
+        visitOrder = new int[N + 1];
 
-		// 리스트 넣어주기
-		for (int i = 1; i <= N; i++) {
-			graph[i] = new ArrayList<>();
-		}
+        for (int i = 1; i <= N; i++) {
+            graph[i] = new ArrayList<>();
+        }
 
-		// 그래프 배열에 노드,엣지 정보 입력
-		for (int i = 0; i < M; i++) {
-			st = new StringTokenizer(br.readLine());
-			int u = Integer.parseInt(st.nextToken());
-			int v = Integer.parseInt(st.nextToken());
+        for (int i = 0; i < M; i++) {
+            st = new StringTokenizer(br.readLine());
+            int u = Integer.parseInt(st.nextToken());
+            int v = Integer.parseInt(st.nextToken());
 
-			graph[u].add(v);
-			graph[v].add(u);
-		}
+            graph[u].add(v);
+            graph[v].add(u);
+        }
 
-		for (int i = 1; i <= N; i++) {
-			Collections.sort(graph[i]);
-		}
+        for (int i = 1; i <= N; i++) {
+            Collections.sort(graph[i]);
+        }
 
-		bfs(R);
+        bfs(R);
 
-		for (int i = 1; i <= N; i++) {
-			if (visitOrder[i] == -1) {
-				System.out.println(0);
-			} else {
-				System.out.println(visitOrder[i]);
-			}
-		}
+        StringBuilder result = new StringBuilder();
+        for (int i = 1; i <= N; i++) {
+            if (visitOrder[i] == -1) {
+                result.append(0).append("\n");
+            } else {
+                result.append(visitOrder[i]).append("\n");
+            }
+        }
 
-	}
+        System.out.print(result);
+    }
 
-	public static void bfs(int node) {
+    public static void bfs(int node) {
+        for (int i = 1; i < visitOrder.length; i++) {
+            visitOrder[i] = -1;
+        }
 
-		for (int x : visitOrder) {
-			x = -1;
-		}
-		int order = 1;
-		visited[node] = true;
-		visitOrder[node] = order;
-		queue.add(node);
+        int order = 1;
+        visited[node] = true;
+        visitOrder[node] = order;
+        queue.add(node);
 
-		while (!queue.isEmpty()) {
-			int u = queue.poll();
-			for (int nearnode : graph[u]) {
-				if (!visited[nearnode]) {
-					order++;
-					visited[nearnode] = true;
-					visitOrder[nearnode] = order;
-					queue.add(nearnode);
-				}
-			}
-		}
-	}
+        while (!queue.isEmpty()) {
+            int u = queue.poll();
+            for (int nearnode : graph[u]) {
+                if (!visited[nearnode]) {
+                    order++;
+                    visited[nearnode] = true;
+                    visitOrder[nearnode] = order;
+                    queue.add(nearnode);
+                }
+            }
+        }
+    }
 }
