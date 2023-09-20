e8cf45683b0cc2d3b385550a3d73acf9188eecc5
pado7sea <140683237+pado7sea@users.noreply.github.com>
2023-09-20 16:39:39 +0900

    [D4] Title: 서로소 집합, Time: 3,596 ms, Memory: 140,264 KB -BaekjoonHub

---
+++ SWEA/D4/3289. 서로소 집합/README.md
@@ -0,0 +1,11 @@
+# [D4] 서로소 집합 - 3289 
+
+[문제 링크](https://swexpertacademy.com/main/code/problem/problemDetail.do?contestProbId=AWBJKA6qr2oDFAWr) 
+
+### 성능 요약
+
+메모리: 140,264 KB, 시간: 3,596 ms, 코드길이: 1,489 Bytes
+
+
+
+> 출처: SW Expert Academy, https://swexpertacademy.com/main/code/problem/problemList.do
---
+++ SWEA/D4/3289. 서로소 집합/서로소 집합.java
@@ -0,0 +1,65 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Solution {
+
+	static int[] p; // 대표를 저장할 배열
+
+	static int findset(int x) {
+		if (x != p[x])
+			p[x] = findset(p[x]);
+		return p[x];
+	}
+
+	static void union(int x, int y) {
+		 p[findset(y)] = findset(x); // x의 대표를 y의 대표로 넣겠다. rank고려x
+	}
+
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st;
+
+		int T = Integer.parseInt(br.readLine());
+
+		for (int tc = 1; tc <= T; tc++) {
+			st = new StringTokenizer(br.readLine());
+			int n = Integer.parseInt(st.nextToken());// 정점의 개수 
+			int m = Integer.parseInt(st.nextToken());// 연산의 개수
+
+			p = new int[n+1]; // 정점은 1부터 시작
+			
+			// make-set
+			for (int i = 0; i <= n; i++) {
+				p[i] = i;
+			}
+
+			// 두 원소가 같은 집합에 포함되어 있는지 여부를 저장할 변수
+			String res = "";
+
+			for (int i = 0; i < m; i++) {
+				st = new StringTokenizer(br.readLine());
+				int case0or1 = Integer.parseInt(st.nextToken());
+				int a = Integer.parseInt(st.nextToken());
+				int b = Integer.parseInt(st.nextToken());
+
+				if (case0or1 == 0) {
+					// 합집합
+					union(a, b);
+					
+				} else {
+					// 두 원소가 같은 집합에 포함되어 있는지 확인
+					if(findset(a) == findset(b)) {
+						res+="1";
+					}else {
+						res+="0";
+					}
+				}
+
+			}
+
+			System.out.println("#" + tc + " " + res);
+
+		} // tc
+	}// main
+}
