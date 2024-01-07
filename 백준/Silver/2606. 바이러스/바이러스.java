f87354f23d4aafe6e7df0f14044c499241028ec3
pado7sea <140683237+pado7sea@users.noreply.github.com>
2024-01-07 23:39:28 +0900

    [Silver III] Title: 바이러스, Time: 124 ms, Memory: 14180 KB -BaekjoonHub

--- 백준/Silver/2606. 바이러스/바이러스.java
+++ 백준/Silver/2606. 바이러스/바이러스.java
@@ -14,8 +14,8 @@ public class Main {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st;
 
-		N = Integer.parseInt(br.readLine()); // 컴퓨터의 수(정점의 수)
-		M = Integer.parseInt(br.readLine()); // 컴퓨터 쌍의 수(간선의 수)
+		N = Integer.parseInt(br.readLine());
+		M = Integer.parseInt(br.readLine());
 
 		adjList = new ArrayList[N + 1];
 		visited = new boolean[N+1];
@@ -40,14 +40,13 @@ public class Main {
 	}
 
 	private static void dfs(int node) {
-		//방문표시
+
 		visited[node] = true;
 		
-		//현재 노드와 연결되어있고 방문한 적 없는 인접 노드들에 대해 dfs 실행
 		for(int adj : adjList[node]) {
 			if(!visited[adj]) {
 				dfs(adj);
-				count++; //감염시킨 컴퓨터의 수 1 증가
+				count++;
 			}
 		}
 	}
