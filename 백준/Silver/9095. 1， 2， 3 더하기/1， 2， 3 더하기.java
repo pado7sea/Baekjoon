6ec32f53d9d184a3d4f41019179979eda6afdf1d
pado7sea <140683237+pado7sea@users.noreply.github.com>
2024-01-18 21:50:29 +0900

    [Silver III] Title: 1, 2, 3 더하기, Time: 204 ms, Memory: 17704 KB -BaekjoonHub

--- 백준/Silver/9095. 1， 2， 3 더하기/1， 2， 3 더하기.java
+++ 백준/Silver/9095. 1， 2， 3 더하기/1， 2， 3 더하기.java
@@ -1,23 +1,25 @@
 import java.util.Scanner;
 
 public class Main {
-	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
-		int t = sc.nextInt();
-		for (int tc = 1; tc <= t; tc++) {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
 
-			int n = sc.nextInt();
+        int[] d = new int[12];
 
-			int[] d = new int[11];
+        d[1] = 1;
+        d[2] = 2;
+        d[3] = 4;
 
-			d[1] = 1;
-			d[2] = 2;
-			d[3] = 4;
+        for (int i = 4; i <= 11; i++) {
+            d[i] = d[i - 1] + d[i - 2] + d[i - 3];
+        }
 
-			for (int i = 4; i <= n; i++) {
-				d[i] = d[i - 1] + d[i - 2] + d[i - 3];
-			}
-			System.out.println(d[n]);
-		}
-	}
+        int t = sc.nextInt();
+        for (int tc = 1; tc <= t; tc++) {
+
+            int n = sc.nextInt();
+
+            System.out.println(d[n]);
+        }
+    }
 }
