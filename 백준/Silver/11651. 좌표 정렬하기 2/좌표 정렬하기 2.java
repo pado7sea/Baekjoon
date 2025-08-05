ecb80ba5ab378ce1fbb37c6345ed1bf3ae00edf7
정혜진 <140683237+pado7sea@users.noreply.github.com>
2025-08-05 19:46:22 +0900

    [Silver V] Title: 좌표 정렬하기 2, Time: 1768 ms, Memory: 187508 KB -BaekjoonHub

--- 백준/Silver/11651. 좌표 정렬하기 2/좌표 정렬하기 2.java
+++ 백준/Silver/11651. 좌표 정렬하기 2/좌표 정렬하기 2.java
@@ -1,29 +1,32 @@
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.util.Arrays;
-import java.util.StringTokenizer;
+import java.util.*;
+
+class Point {
+    int x, y;
+    Point(int x, int y){
+        this.x = x;
+        this.y = y;
+    }
+}
 
 public class Main {
-	public static void main(String[] args) throws Exception {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
-		int N = Integer.parseInt(br.readLine());
-		int[][] arr = new int[N][2];
-		for (int i = 0; i < N; i++) {
-			StringTokenizer st = new StringTokenizer(br.readLine());
-			arr[i][0] = Integer.parseInt(st.nextToken());
-			arr[i][1] = Integer.parseInt(st.nextToken());
-		}
-		//람다식
-		Arrays.sort(arr, (a, b) -> {
-			if (a[1] == b[1]) {
-				return a[0] - b[0];
-			}
-			return a[1] - b[1];
-		});
-
-		for (int i = 0; i < N; i++) {
-			System.out.println(arr[i][0] + " " + arr[i][1]);
-		}
-	}
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        List<Point> list = new ArrayList<>();
+        
+        for(int i= 0; i<n;i++){
+            list.add(new Point(sc.nextInt(),sc.nextInt()));
+        }
+        
+        Collections.sort(list, (a,b)->{
+           if(a.y != b.y){
+               return a.y- b.y;
+           } return a.x - b.x;
+        });
+        
+        for(Point p : list){
+            System.out.println(p.x+" "+p.y);
+        }
+        
+    }
 }
