6a3c15b25f47865c013def03eda31977c1a7735f
정혜진 <140683237+pado7sea@users.noreply.github.com>
2025-08-10 01:50:35 +0900

    [Gold V] Title: 평범한 배낭, Time: 248 ms, Memory: 56900 KB -BaekjoonHub

--- 백준/Gold/12865. 평범한 배낭/평범한 배낭.java
+++ 백준/Gold/12865. 평범한 배낭/평범한 배낭.java
@@ -1,38 +1,31 @@
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.util.Arrays;
-import java.util.StringTokenizer;
+import java.util.*;
 
-public class Main {
-    public static void main(String[] args) throws Exception {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st;
-
-        st = new StringTokenizer(br.readLine());
-        int N = Integer.parseInt(st.nextToken());  // 물품의 수
-        int K = Integer.parseInt(st.nextToken());  // 버틸 수 있는 무게
-
-        int[] dp = new int[K + 1];  // 배낭의 각 무게에 대한 최대 가치를 저장하는 배열
-        int[][] arr = new int[N + 1][2];  // 물건의 무게와 가치를 저장하는 배열
-
-        for (int i = 1; i <= N; i++) {
-            st = new StringTokenizer(br.readLine());
-            arr[i][0] = Integer.parseInt(st.nextToken());  // 물건의 무게
-            arr[i][1] = Integer.parseInt(st.nextToken());  // 물건의 가치
+class Main {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+        int K = sc.nextInt();
+        int[] W = new int[N+1];
+        int[] V = new int[N+1];
+        int[][] dp = new int[N+1][K+1];
+        for(int i = 1;i<=N;i++){
+            W[i] = sc.nextInt();
+            V[i] = sc.nextInt();
         }
-
-        // dp 배열 초기화
-        for (int i = 0; i <= K; i++) {
-            dp[i] = 0;  // 배낭의 무게가 0일 때 가치는 0
-        }
-
-        // 물건을 하나씩 고려하면서 최대 가치를 계산
-        for (int i = 1; i <= N; i++) {
-            for (int j = K; j >= arr[i][0]; j--) {
-                // 현재 물건을 배낭에 담을 수 있는 경우와 담지 않을 경우 중에서 최대 가치 선택
-                dp[j] = Math.max(dp[j], dp[j - arr[i][0]] + arr[i][1]);
+        
+        for(int i = 1;i<=N;i++){
+            for(int w=0;w<=K;w++){
+                dp[i][w] = dp[i-1][w];
+                if(w>=W[i]){
+                    dp[i][w] = Math.max(dp[i-1][w], dp[i-1][w-W[i]]+V[i]);
+                }
+                
             }
+            
         }
-        System.out.println(dp[K]);  // 배낭의 무게 제한이 K일 때 얻을 수 있는 최대 가치 출력
+        
+        System.out.print(dp[N][K]);
+        
     }
+    
 }
